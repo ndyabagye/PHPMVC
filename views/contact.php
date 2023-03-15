4b2ef6d52935d4411f78ddc0975703bbcdf98273
Ndyabagye Henry <ndyabagyehenrytusi@gmail.com>
2023-03-15 17:00:36 +0300

    Introduced routes, views, and html layouts

---
+++ views/contact.php
@@ -0,0 +1,17 @@
+
+<form>
+    <div class="grid gap-4 my-6 md:grid-cols-3">
+        <div>
+            <label for="first_name">First name</label>
+            <input type="text" id="first_name">
+        </div>
+        <div>
+            <label for="last_name" >Last name</label>
+            <input type="text" id="last_name" >
+        </div>
+        <div>
+            <label for="company" >Company</label>
+            <input type="text" >
+        </div>
+    <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+</form>
